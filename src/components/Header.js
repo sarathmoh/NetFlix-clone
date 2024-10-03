199778f032ef88edd0006221d273c22bda4b4330
sarath mohan <esarathmohan@gmail.com>
2024-10-03 11:44:32 +0530

    firebase configuration done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const signoutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
-    <div className=" absolute px-2 py-2 bg-gradient-to-b from-black z-10">
+    <div className=" absolute px-2 py-2 bg-gradient-to-b from-black z-10 w-full flex justify-between">
       <img
         className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      {user && (
+        <div className="flex p-8">
+          <img className=" mx-5 w-12 h-12" src={user?.photoURL} />
+          <button className="text-white" onClick={signoutHandler}>
+            Sign out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
